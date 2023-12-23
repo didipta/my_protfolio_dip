e44e80c76bd90607e43222b18bc1618da43908e1
didipta <66532706+didipta@users.noreply.github.com>
2023-12-23 16:17:37 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,12 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "winter"
+      // ,"dracula"
+  ],
+    // themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
 }
